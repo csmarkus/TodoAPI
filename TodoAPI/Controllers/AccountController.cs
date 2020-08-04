661d3db38fbb9f0dd992e6f097afbbceb76f4b16
Craig <infected.shadow0@gmail.com>
2020-08-03 22:14:58 -0400

    Added Endpoint for Registration
    * Removed autogenerated weatherforecast crap
    * Created Account Controller
    * Added endpoint for registration
    * Fixed some launch settings to avoid going to an endpoint that no longer exists

---
+++ TodoAPI/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TodoAPI.Models;
+using TodoAPI.Services;
+
+namespace TodoAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]UserRegistration model)
+        {
+            try
+            {
+                _userService.Create(model);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
